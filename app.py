68a81a31593de12ce990413d92babec595d23715
Prat <y.pratima@ymail.com>
2025-06-10 01:19:24 +0530

    Initial commit

--- app.py
+++ app.py
@@ -82,3 +82,9 @@ def add_ship():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
